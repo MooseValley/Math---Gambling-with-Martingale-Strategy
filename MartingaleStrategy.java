0d4ec996a1f72c989741b3b06248da7a3ebb8fa7
MooseValley <Moose_Software@yahoo.com.au>
2021-05-17 11:35:37 +1000

    v0.02
    v0.02 - formatting, comment header

--- MartingaleStrategy.java
+++ MartingaleStrategy.java
@@ -1,3 +1,12 @@
+/*
+     Author: Moose OMalley
+       Date: 17-May-2021
+     GitHub: https://github.com/MooseValley/Math---Gambling-with-Martingale-Strategy
+    License: MIT License.  (Use code for whatever you want, but give me credit).
+Description: Explore Martingale Strategy betting strategy for American and European Roulette Wheels.
+Inspiration: Gambling with the Martingale Strategy - Numberphile, https://www.youtube.com/watch?v=zTsRGQj6VT4
+
+*/
 import java.security.SecureRandom;
 
 public class MartingaleStrategy
@@ -13,11 +22,13 @@ public class MartingaleStrategy
 
    public static double currentBankBalance;
    public static double currentBetAmount;
+   public static double maxBankBalance;
+
+   public static SecureRandom generator = new SecureRandom();
 
 
-   public static void main (String[] args)
+   public static int playUntilBankrupt ()
    {
-      SecureRandom generator = new SecureRandom();
       int    iterations = 0;
       String resultStr  = "";
 
@@ -50,10 +61,57 @@ public class MartingaleStrategy
             currentBetAmount   = 2 * currentBetAmount;
          }
 
+         if (maxBankBalance < currentBankBalance)
+            maxBankBalance = currentBankBalance;
+
          //System.out.println (iterations + ". " +  resultStr + "   Balance: " + currentBankBalance);
 
       }
 
-      System.out.println ("Game Over: " + iterations + ". " +  "   Balance: " + currentBankBalance);
+      //System.out.println ("-> Bankrupt after: " + iterations + " hands. " +
+      //                    "   Balance: " + String.format ("%.2f", currentBankBalance) );
+
+      return iterations;
+   }
+
+
+   public static void main (String[] args)
+   {
+      final int NUM_GAMES      = 100_000;
+
+      int    totalIterations = 0;
+      int    minIterations   = Integer.MAX_VALUE;
+      int    maxIterations   = 0;
+      double avgIterations   = 0.0;
+
+      maxBankBalance = 0.0;
+
+      System.out.println ();
+      System.out.println ("Martingale Strategy for " + String.format ("%,d", NUM_GAMES) + " games of roulette:" );
+      System.out.println ();
+
+      for (int k = 0; k < NUM_GAMES; k++)
+      {
+         int iterations = playUntilBankrupt ();
+
+         totalIterations += iterations;
+
+         if (minIterations > iterations)
+            minIterations = iterations;
+
+         if (maxIterations < iterations)
+            maxIterations = iterations;
+      }
+
+      avgIterations = totalIterations / NUM_GAMES;
+
+      System.out.println ();
+      System.out.println ("Hands to go Bankrupt: "      +
+                          ", Min: "     + String.format ("%,d",  minIterations) +
+                          ", Average: " + String.format ("%.0f", avgIterations) +
+                          ", Max: "     + String.format ("%,d",  maxIterations) +
+                          ", Higest bank balance achieved: $" + String.format ("%.2f", maxBankBalance)
+                          );
+
    }
 }
